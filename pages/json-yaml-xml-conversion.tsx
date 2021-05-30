029f6fb514f334700cb31e0712485ee3f576acef
Deepak Uniyal <deepakuniyal@microsoft.com>
2021-05-30 17:21:13 +0530

    Revert "Render static pages and next export on build"
    This reverts commit 92e435dd360c8c7f1562bcfc2c2cac1411c50afd.

--- pages/json-yaml-xml-conversion.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { ROUTE_TO_UNIQUE_UTILITIES } from "../constants";
-import HTML from "./_html";
-
-export default function JsonYamlXmlConverter(): JSX.Element {
-  return <HTML url={ROUTE_TO_UNIQUE_UTILITIES["json-yaml-xml-conversion"]} />;
-}
