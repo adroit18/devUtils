77b2c64a002bcdb58b8da9444e1a500084aa359f
Deepak Uniyal <deepakuniyal@microsoft.com>
2022-01-06 19:55:52 +0530

    Index Page change

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,5 +3,5 @@ import { ROUTE_TO_UNIQUE_UTILITIES } from "../constants";
 import HTML from "./_html";
 
 export default function Home(): JSX.Element {
-  return <HTML url={ROUTE_TO_UNIQUE_UTILITIES["json-format-or-validate"]} />;
+  return <HTML url={ROUTE_TO_UNIQUE_UTILITIES["text-difference"]} />;
 }
